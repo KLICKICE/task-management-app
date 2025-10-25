08fdd0ab4ef8280d0ab56bca8ffbe2efff052349
KLICKICE <159444914+KLICKICE@users.noreply.github.com>
2025-10-26 01:07:57 +0300

    Feature/project comment label attachment entities (#10)
    * Added new entities, changed task entity

---
+++ src/main/java/mate/academy/taskmanagementapp/model/ProjectStatus.java
@@ -0,0 +1,33 @@
+package mate.academy.taskmanagementapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "project-statuses")
+public class ProjectStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusProject statusProject;
+
+    public enum StatusProject {
+        INITIATED,
+        IN_PROGRESS,
+        COMPLETED
+    }
+}
