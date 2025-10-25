87e89ab127196864cca06fa0c67c64723c528895
KLICKICE <ledmaskiight@gmail.com>
2025-10-26 01:05:42 +0300

    Added new entities, changed task entity

---
+++ src/main/java/mate/academy/taskmanagementapp/model/Attachment.java
@@ -0,0 +1,46 @@
+package mate.academy.taskmanagementapp.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "attachments")
+public class Attachment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @Column(nullable = false, unique = true)
+    private String dropboxFileId;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private LocalDateTime uploadDate;
+
+    @PrePersist
+    public void setUploadDate() {
+        if (uploadDate == null) {
+            uploadDate = LocalDateTime.now();
+        }
+    }
+}
---
+++ src/main/java/mate/academy/taskmanagementapp/model/Comment.java
@@ -0,0 +1,47 @@
+package mate.academy.taskmanagementapp.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @Column(nullable = false, length = 1000)
+    private String text;
+
+    @Column(nullable = false)
+    private LocalDateTime timeStamp;
+
+    @PrePersist
+    public void setTimestamp() {
+        if (timeStamp == null) {
+            timeStamp = LocalDateTime.now();
+        }
+    }
+}
