3499e87e3a123305f6537a3790e58df151a942ac
KLICKICE <ledmaskiight@gmail.com>
2025-10-28 18:04:16 +0200

    Fixed Project

--- src/main/java/mate/academy/taskmanagementapp/controller/ProjectController.java
+++ src/main/java/mate/academy/taskmanagementapp/controller/ProjectController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import mate.academy.taskmanagementapp.dto.project.CreateProjectRequestDto;
 import mate.academy.taskmanagementapp.dto.project.ProjectDto;
 import mate.academy.taskmanagementapp.dto.project.ProjectUpdateDto;
-import mate.academy.taskmanagementapp.model.project.ProjectStatus;
 import mate.academy.taskmanagementapp.service.project.ProjectService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Project management", description = "Operations related to projects")
@@ -25,42 +23,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/projects")
 @RequiredArgsConstructor
 public class ProjectController {
+
     private final ProjectService projectService;
 
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @PostMapping
-    ProjectDto createProject(@RequestBody CreateProjectRequestDto dto) {
+    public ProjectDto createProject(@RequestBody CreateProjectRequestDto dto) {
         return projectService.createProject(dto);
     }
 
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping("/{id}")
-    ProjectDto getProjectById(@PathVariable Long id) {
+    public ProjectDto getProjectById(@PathVariable Long id) {
         return projectService.getProjectById(id);
     }
 
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    @GetMapping("/by-owner")
-    List<ProjectDto> findAllByOwner(@RequestParam Long ownerId) {
-        return projectService.findAllByOwner(ownerId);
-    }
-
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    @GetMapping("/by-status")
-    List<ProjectDto> findAllByStatus(@RequestParam ProjectStatus.StatusProject status) {
-        return projectService.findAllByStatus(status);
+    @GetMapping("/my-projects")
+    public List<ProjectDto> getUserProjects() {
+        return projectService.getUserProjects();
     }
 
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @PutMapping("/{id}")
-    ProjectDto updateProject(@PathVariable Long id,
-                                    @RequestBody ProjectUpdateDto dto) {
+    public ProjectDto updateProject(@PathVariable Long id, @RequestBody ProjectUpdateDto dto) {
         return projectService.updateProject(id, dto);
     }
 
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @DeleteMapping("/{id}")
-    void deleteProject(@PathVariable Long id) {
+    public void deleteProject(@PathVariable Long id) {
         projectService.deleteProject(id);
     }
 }
